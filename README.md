66a4157622386e7050ee091d1ea272b85765d66d
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-15 21:21:51 -0400

    badly drawn arrow and click handlers

---
+++ README.md
@@ -0,0 +1,8 @@
+# Carousel demo for Zag Interactive.
+
+## Setup
+1. install dot net core
+2. cd demo
+3. dotnet run
+4. localhost:5000
+
--- demo/wwwroot/js/site.js
+++ demo/wwwroot/js/site.js
@@ -1,1 +1,9 @@
 ﻿// Write your JavaScript code.
+
+$("#left").click(function(){
+    alert("clicked left");
+})
+
+$("#right").click(function(){
+    alert("right clicked");
+})
