51865320e375aa977465774a4f66bd744bd39a04
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-16 05:50:35 -0400

    working left button

--- demo/wwwroot/js/site.js
+++ demo/wwwroot/js/site.js
@@ -1,15 +1,28 @@
 ﻿
 $("#left").click(function(){
+    // debugger;
+    var $cur = $(".carousel-item.active");
+    var $previous = $cur.prev();
     
+    if($previous.length == 0){
+        //set last as active
+        var $parent = $cur.parent();
+        var $last = $parent.find(".carousel-item:last-child");
+        $last.addClass("active");
+    } else {
+        $previous.addClass("active");
+    }
+    $cur.removeClass("active");
 })
 
+//this could be simplified to a single function that takes selectors..
+//leaving it as is because its a demo
 $("#right").click(function(){
 
     var $cur  = $(".carousel-item.active");
     var $next = $cur.next();  
 
     if($next.length == 0){
-        // debugger;
         //There are no more items, restart to first.
         var $parent = $cur.parent();
         var $first = $parent.find(".carousel-item:first-child");
