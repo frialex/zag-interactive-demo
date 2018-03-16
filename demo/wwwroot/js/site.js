a6f4c06e40fdc64eb07908e2974b47eaa410f65d
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-15 21:32:18 -0400

    semi-working right arrow click

--- demo/wwwroot/js/site.js
+++ demo/wwwroot/js/site.js
@@ -1,9 +1,24 @@
 ﻿// Write your JavaScript code.
 
 $("#left").click(function(){
-    alert("clicked left");
+    
 })
 
 $("#right").click(function(){
-    alert("right clicked");
+    //debugger;
+
+    //find .carousel-item.active  next sibling
+    //remove .active from current and add it to sibling
+
+    var $cur  = $(".carousel-item.active");
+    var $next = $cur.next();
+    //var $next = $(".carousel-item.active .carousel-item")
+
+    //TODO: Need to add logic to check if next is null, if so
+    //find the find .carousel-item of the current active one.   
+
+    $cur.removeClass("active");
+    $next.addClass("active");
+
+    
 })
